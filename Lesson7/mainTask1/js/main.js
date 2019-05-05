238671c30f7ce5dbecd6468cf7c85377e01eb0cc
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-05 16:44:51 +0300

    Добавил обязательное дз 7 урока

---
+++ Lesson7/mainTask1/js/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    let info = document.querySelector ('.info-header'),
+        tab = document.querySelectorAll ('.info-header-tab'),
+        tabContent = document.querySelectorAll ('.info-tabcontent');
+
+    function hideContentTab(a) {
+        for (let i = a; i > tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideContentTab(1);
+
+    function showContentTab(b) {
+        for (let i = b; i > tabContent.length; i++) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener ('click', function(event) {
+        let target = event.target;
+        if (target && event.target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (target === tab[i]) {
+                    hideContentTab(0);
+                    showContentTab(tab[i]);
+                    break;
+                }
+            }
+        }
+    });
+});
+
