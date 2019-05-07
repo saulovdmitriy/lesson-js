88e9d2c7bf8473a54bb8a0b7e38cbb5fbf164637
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-05-08 00:05:44 +0300

    Сделал вывод данных двумя способами

--- Lesson9/mainTask2/script.js
+++ Lesson9/mainTask2/script.js
@@ -1,20 +1,14 @@
 let age = document.getElementById('age');
 
-let user = {
-
-    value: age.value
-    
-};
-
 function showUser(surname, name) {
 
-    age.addEventListener ('change', function() {
-
-        alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-
-    });
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 
 }
+//С помощью apply
+// showUser.apply(age, ['Иванов', 'Иван']);
 
-showUser('Иванов', 'Иван');
- 
+//С помощью bind
+let a = showUser.bind(age);
+
+a('Иванов ', 'Иван');
